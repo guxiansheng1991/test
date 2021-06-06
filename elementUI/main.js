cfbd06cb974cd02905cba5fc62f02bce4b543571
guxiansheng1991 <2943887177@qq.com>
2021-06-06 23:15:21 +0800

    回文

---
+++ elementUI/main.js
@@ -0,0 +1,15 @@
+
+window.onload = function () {
+    var Main = {
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+    var Ctor = Vue.extend(Main)
+    new Ctor().$mount('#app')
+}
